e26daa6b36ae0a7261dd5ab312535e0645725a4f
Rostyslav <rostislav.tsekhmistro@gmail.com>
2020-04-06 23:07:31 +0300

    Initial commit (only top style)

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="top_block">
+      <div class="header">
+        <h3>Startup</h3>
+        <ul class="navbar">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Works</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Clients</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="main_banner">
+        <h1>Welcome to startup</h1>
+        <span>Your Favourite Creative Agency Template</span>
+        <button>get started</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+@import "sass/style"
+</style>
